96ea0c6359148410cd0ea96f42bc6461a5afb599
wilder10 <fabien.poher45@gmail.com>
2017-09-07 22:08:36 +0200

    A la Wild Code School on code en chaussette

---
+++ Wild.php
@@ -0,0 +1,24 @@
+From 2a3c237f092ee09b78fd8ead3ce68bb1bcddb2d6 Mon Sep 17 00:00:00 2001
+From:   <Ð½z®>
+Date: Tue, 5 Sep 2017 13:43:07 +0200
+Subject: [PATCH] first commit
+
+---
+ README.md | 4 ++++
+ 1 file changed, 4 insertions(+), 0 deletions(-)
+
+diff --git a/README.md b/README.md
+index 6b2d371..74bbf43 100644
+--- a/README.md
++++ b/README.md
+@@ -7,3 +7,7 @@ git init
+ echo # https://github.com/jakeasmith/commit
+ https://github.com/rishabhp/pappu-pakia.git
+ # https://github.com/jakeasmith/commit
++# https://github.com/jakeasmith/commit
++# https://github.com/FarhadG/init
++# https://github.com/WildCodeSchool/clmystery.git
++# Boostrap
+--
+libgit2 0.24.0
+
